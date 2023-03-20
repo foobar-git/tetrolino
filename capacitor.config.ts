29ae10f2c755b452c975521deb3664a272425e7f
foobar-git <foobar.masterfoo@gmail.com>
2023-03-20 17:40:58 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Tetrolino',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
